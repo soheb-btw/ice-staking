5ce155a2531503d9390e32ad1d63ff04e6979b55
Soheb Attar <soheb.dev@gmail.com>
2025-01-27 18:04:17 +0530

    deployment files

--- src/components/resource-card.tsx
+++ src/components/resource-card.tsx
@@ -6,15 +6,20 @@ interface CardData {
     date: string;
 }
 const ResourceCard = ({ cardData }: { cardData: CardData }) => {
+    const imageUrl = cardData.imageUrl;
     return (
         <div className="w-[272px] bg-white rounded-2xl flex flex-col">
-            <Image className="rounded-tl-2xl rounded-tr-2xl" src={cardData.imageUrl} width={272} height={279} alt={'resource card image'} />
+            <Image className="rounded-tl-2xl rounded-tr-2xl" src={imageUrl} width={272} height={279} alt={'resource card image'} />
             <div className="flex flex-col gap-3 p-6">
                 <div className="text-[#25abe2] text-sm font-bold">{cardData.date}</div>
                 <div className="text-[#092835] text-xl leading-6 font-bold overflow-hidden text-ellipsis whitespace-break-spaces line-clamp-2">{cardData.title}</div>
             </div>
         </div>
     )
+}
+
+export async function generateStaticParams() {
+    return [{imageUrl: '/assets/resources-1.png'}, {imageUrl: '/assets/resources-2.png'}, {imageUrl: '/assets/resources-3.png'}, {imageUrl: '/assets/resources-4.png'}]
 }
 
 export default ResourceCard;
